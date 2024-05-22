75c4753b04b3ced924f34e59013b9a8360fde885
LeoSouza64 <leo.oliveiraadv1@gmail.com>
2024-05-22 20:00:08 +0100

    primeiro commit

---
+++ assets/JavaScript/script.js
@@ -0,0 +1,32 @@
+// Rolagem suave da barra de navegação
+
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
+
+// Botão de voltar ao topo
+
+document.addEventListener("DOMContentLoaded", function () {
+  var backToTopButton = document.getElementById("back-to-top");
+
+  window.addEventListener("scroll", function () {
+    if (window.scrollY > 300) {
+      backToTopButton.classList.add("show");
+    } else {
+      backToTopButton.classList.remove("show");
+    }
+  });
+
+  backToTopButton.addEventListener("click", function () {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  });
+});
